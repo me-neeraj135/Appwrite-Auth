951f6db5dd1a470bf3926190e7a284d47fa6aec1
nkr <me.neeraj1455@gmail.com>
2024-01-26 17:46:11 +0530

    appwrite authentication

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Home from './screens/Home';
+
+function App() {
+  return (
+    <View>
+      <Home />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
